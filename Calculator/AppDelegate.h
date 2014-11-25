ea46cff5a951c3cd05cbdd8c55d6927728e81c39
Eude Kinsley Lesperance <Kinsley@80-e6-50-1f-9c-78.wlan.lsu.edu>
2014-11-25 17:28:36 -0600

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Eude Kinsley Lesperance on 11/25/14.
+//  Copyright (c) 2014 Qlovur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Eude Kinsley Lesperance on 11/25/14.
+//  Copyright (c) 2014 Qlovur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
