79b06a78e5d6b09d1614d2718f649c3ce3db0e95
Eude Kinsley Lesperance <Kinsley@80-e6-50-1f-9c-78.wlan.lsu.edu>
2014-11-25 20:23:45 -0600

    basic calc logic

--- Calculator/ViewController.h
+++ Calculator/ViewController.h
@@ -9,7 +9,20 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    int total;
+    int mode;
+    NSString *valueString;
+    IBOutlet UILabel *label;
+    BOOL lastButtonWasMode;
+}
 
+- (IBAction)tappedClear:(id)sender;
+- (IBAction)tappedNumber:(UIButton* )btn;
+- (IBAction)tappedPlus:(id)sender;
+- (IBAction)tappedMinus:(id)sender;
+- (IBAction)tappedMultiply:(id)sender;
+- (IBAction)tappedEquals:(id)sender;
 
 @end
 
--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -17,11 +17,91 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    valueString = @"";
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)tappedClear:(id)sender
+{
+    total = 0;
+    valueString = @"";
+    label.text = @"0";
+}
+
+- (IBAction)tappedNumber:(UIButton* )btn
+{
+    int num = (int)btn.tag;
+    if (num == 0 && total == 0) {
+        return;
+    }
+    if (lastButtonWasMode) {
+        lastButtonWasMode = NO;
+        valueString = @"";
+    }
+    NSString *numAsString = [NSString stringWithFormat:@"%i", num];
+    valueString = [valueString stringByAppendingString:numAsString];
+    NSNumberFormatter *formatter = [NSNumberFormatter new];
+    [formatter setNumberStyle:NSNumberFormatterDecimalStyle];
+    NSNumber *n =[formatter numberFromString:valueString];
+    label.text = [formatter stringFromNumber:n];
+    if (total == 0) {
+        total = [valueString intValue];
+    }
+    
+}
+
+- (IBAction)tappedPlus:(id)sender
+{
+    [self setMode: 1];
+}
+
+- (IBAction)tappedMinus:(id)sender
+{
+    [self setMode: -1];
+}
+
+- (IBAction)tappedMultiply:(id)sender
+{
+    [self setMode: 2];
+}
+
+- (IBAction)tappedEquals:(id)sender
+{
+    if (mode == 0) {
+        return;
+    }
+    if (mode == 1) {
+        total += [valueString intValue];
+    }
+    if (mode == -1) {
+        total -= [valueString intValue];
+    }
+    if (mode == 2) {
+        total *= [valueString intValue];
+    }
+    valueString = [NSString stringWithFormat:@"%i",total];
+    NSNumberFormatter *formatter = [NSNumberFormatter new];
+    [formatter setNumberStyle:NSNumberFormatterDecimalStyle];
+    NSNumber *n =[formatter numberFromString:valueString];
+    label.text = [formatter stringFromNumber:n];
+    mode = 0;
+}
+
+- (void) setMode:(int) m
+{
+    if (total == 0) {
+        return;
+    }
+    mode = m;
+    lastButtonWasMode = YES;
+    total = [valueString intValue];
+    
 }
 
 @end
